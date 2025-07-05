fca2deaad7a1046cef997387953fe5e272e16bb1
Sajeeb <sajeeb2186@gmail.com>
2025-07-05 11:55:54 +0600

    if else and switch case

--- src/com/ok/javalearning/datatypes/test.java
+++ src/com/ok/javalearning/datatypes/test.java
@@ -6,10 +6,56 @@ public class test {
 
 
 
-        String text="hello world";
-        System.out.println(text.toUpperCase());
-        System.out.println(text.length());
-        System.out.println(text.charAt(1));
+//        String text="hello world";
+//        System.out.println(text.toUpperCase());
+//        System.out.println(text.length());
+//        System.out.println(text.charAt(1));
+
+
+//        boolean isSunny=true;
+//        boolean isWarm=false;
+//
+//        if(isSunny && isWarm){
+//            System.out.println("beach day");
+//        }
+//        else System.out.println("not a beach day");
+
+        int day=2;
+        String dayname;
+
+        switch (day){
+            case 1:
+                dayname="saturday";
+                break;
+            case 2:
+                dayname="sunday";
+                break;
+            case 3:
+                dayname="Monday";
+                break;
+            case 4:
+                dayname="Tuesday";
+                break;
+            case 5:
+                dayname="Wednesday";
+                break;
+            case 6:
+                dayname="Thursday";
+                break;
+            case 7:
+                dayname="Friday";
+                break;
+            default:
+                dayname="Unknown";
+
+
+        }
+
+
+        System.out.println(dayname);
+
+
+
 
     }
 
