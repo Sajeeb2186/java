1767e975969ef18377c84cf2c7b3bc009e39dff9
Sajeeb <sajeeb2186@gmail.com>
2025-07-05 10:40:43 +0600

    java learning

---
+++ src/com/ok/javalearning/datatypes/test.java
@@ -0,0 +1,13 @@
+package com.ok.javalearning.datatypes;
+
+public class test {
+
+    public static void main(String[] args) {
+
+
+         char a='b';
+        System.out.println((int)a);
+
+    }
+
+}
